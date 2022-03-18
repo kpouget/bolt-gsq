f0422dd6da0818fbf411799a090f5b6d69cb76f1
Kevin Pouget <kpouget@redhat.com>
2022-03-18 08:48:28 +0100

    Update k8s->podman

---
+++ run_website.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+for d in $(cat deployment-bolt.yaml | grep 'hostpath' -A1 | grep -v hostpath | grep path | cut -d: -f2);
+do
+    mkdir $d -p
+done
+
+(cat ../secrets/secrets-configmap.yaml; echo ---; cat config.yaml; echo ---; cat deployment-bolt.yaml) | podman play kube -

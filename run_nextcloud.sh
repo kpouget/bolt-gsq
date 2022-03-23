deb2fce4618e5c8096a9e019c1d45780c7bae241
Kevin Pouget <kpouget@redhat.com>
2022-03-23 22:53:22 +0100

    run_nextcloud.sh: create dirs from deployment-nextcloud.yaml

--- run_nextcloud.sh
+++ run_nextcloud.sh
@@ -1,4 +1,8 @@
 #! /bin/bash
 
-mkdir -p /home/kevin/vayrac/gsq/website-bolt/data/nextcloud/{html,data,db}
+for d in $(cat deployment-nextcloud.yaml | grep 'hostpath' -A1 | grep -v hostpath | grep path | cut -d: -f2);
+do
+    mkdir $d -p
+done
+
 (cat ../secrets/secrets-configmap.yaml; echo ---; cat config.yaml; echo ---; cat deployment-nextcloud.yaml) | podman play kube - "$@"

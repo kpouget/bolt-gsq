630edb6e7b1edb304fe32f0f3f7709a705d6cd36
Kevin Pouget <kpouget@redhat.com>
2022-06-26 15:28:46 +0200

    Add NEXT as a copy of GSQ

--- run_website.sh
+++ run_website.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
-for d in $(cat deployment-bolt.yaml | grep 'hostpath' -A1 | grep -v hostpath | grep path | cut -d: -f2);
+for d in $(cat deployment-bolt{,-next}.yaml | grep 'hostpath' -A1 | grep -v hostpath | grep path | cut -d: -f2);
 do
     mkdir $d -p
 done
 
-(cat ../secrets/secrets-configmap.yaml; echo ---; cat config.yaml; echo ---; cat deployment-bolt.yaml) | podman play kube - "$@"
+(cat ../secrets/secrets-configmap.yaml; echo ---; cat config.yaml; echo ---; cat deployment-bolt.yaml; echo ---; cat deployment-bolt-next.yaml) | podman play kube - "$@"
